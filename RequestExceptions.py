ed249203e795b2b732ff3154807fa32fb9b4fa15
Ben Andrews <benafandrews@gmail.com>
2021-01-30 00:03:37 +0000

    Removed RequestExceptions.py

--- RequestExceptions.py
+++
@@ -1,9 +0,0 @@
-
-class Error404(Exception):
-    def __init__(self, url: str, time: float, msg: str = "Request to {} timed out in {}") -> None:
-        self.url = url
-        self.time = time
-        self.msg = msg.format(url, time)
-        super().__init__(self.msg)
-
-
